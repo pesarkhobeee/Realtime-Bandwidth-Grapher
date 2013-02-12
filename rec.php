6f2f178c98da57aab1313f770dbbf511023f9ec5
Farid Ahmadian <pesarkhobeee@gmail.com>
2013-02-12 12:06:28 +0330

    Correction of executable file path

--- rec.php
+++ rec.php
@@ -3,7 +3,7 @@ include('config.php');
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-$rec = system("ifconfig $interface | grep 'RX bytes' | sed -e 's,.*RX bytes:,,' -e 's, .*,,'");
+$rec = system("/sbin/ifconfig $interface | grep 'RX bytes' | sed -e 's,.*RX bytes:,,' -e 's, .*,,'");
 
 // $time = date('r');
 echo "retry: 1000\n";
